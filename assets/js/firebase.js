a8736bef602cfafb79b1052d528b1bfe21c6d61f
naciyeduygu <duygunaciye86@gmail.com>
2026-06-10 14:53:23 +0300

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,29 +1,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  serverTimestamp
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+import {
+  getFirestore
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "BURAYA_API_KEY",
-  authDomain: "BURAYA_AUTH_DOMAIN",
-  projectId: "BURAYA_PROJECT_ID",
-  storageBucket: "BURAYA_STORAGE_BUCKET",
-  messagingSenderId: "BURAYA_MESSAGING_ID",
-  appId: "BURAYA_APP_ID"
+  apiKey: "AIzaSyC5LhMCjYpj_SHBHcc2_devYC802Wnj0wg",
+  authDomain: "cici-kitap.firebaseapp.com",
+  projectId: "cici-kitap",
+  storageBucket: "cici-kitap.firebasestorage.app",
+  messagingSenderId: "931873884855",
+  appId: "1:931873884855:web:159e9ff1deef46f1b6db32"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export {
-  db,
-  collection,
-  addDoc,
-  getDocs,
-  serverTimestamp
-};
+export const auth = getAuth(app);
+export const db = getFirestore(app);

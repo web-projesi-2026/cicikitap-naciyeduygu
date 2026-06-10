b71c70d4ac693aa5b32506863228f5025a75e49a
naciyeduygu <duygunaciye86@gmail.com>
2026-06-10 14:56:39 +0300

    Create kayit.js

---
+++ assets/js/kayit.js
@@ -0,0 +1,64 @@
+import { auth, db } from "./firebase.js";
+
+import {
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+import {
+  doc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const form = document.getElementById("kayitForm");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const adsoyad =
+      document.getElementById("adsoyad").value;
+
+    const email =
+      document.getElementById("email").value;
+
+    const sifre =
+      document.getElementById("sifre").value;
+
+    const sifreTekrar =
+      document.getElementById("sifreTekrar").value;
+
+    if (sifre !== sifreTekrar) {
+        alert("Şifreler uyuşmuyor!");
+        return;
+    }
+
+    try {
+
+        const userCredential =
+        await createUserWithEmailAndPassword(
+            auth,
+            email,
+            sifre
+        );
+
+        const user = userCredential.user;
+
+        await setDoc(
+          doc(db, "uyeler", user.uid),
+          {
+             adsoyad: adsoyad,
+             email: email,
+             rol: "kullanici",
+             tarih: new Date().toLocaleDateString("tr-TR")
+          }
+        );
+
+        alert("Kayıt başarılı!");
+
+    } catch (error) {
+
+        alert(error.message);
+
+    }
+
+});

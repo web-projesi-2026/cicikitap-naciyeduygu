d8dff40dd2e79e59b0f5622326e5a6e0f2ad3dee
naciyeduygu <duygunaciye86@gmail.com>
2026-06-10 14:57:45 +0300

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,12 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC5LhMCjYpj_SHBHcc2_devYC802Wnj0wg",
